417273f4ab1d03f233d412f9a445ba7456676d71
Kyle Smith <73062855+vertanzil@users.noreply.github.com>
2025-01-23 22:03:52 +0000

    Commit

---
+++ src/main/java/io/github/vertanzil/Main.java
@@ -0,0 +1,7 @@
+package io.github.vertanzil;
+
+public class Main {
+    public static void main(String[] args) {
+    System.out.print(Messages.Message_004.getCode() + " " + Messages.Message_004.getDescription());
+    }
+}
---
+++ src/main/java/io/github/vertanzil/Messages.java
@@ -0,0 +1,37 @@
+package io.github.vertanzil;
+
+public enum Messages {
+    Message_001("0001", "Files not found."),
+    Message_002("0002", "No connection to the database"),
+    Message_003("0003", "Server offline"),
+    Message_004("0004", "Not found"),
+    ;
+
+    private String code;
+    private String description;
+    public static final String ERROR_CODE_PREFIX = "BCV";
+
+    private void Error(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    Messages(String number, String s) {
+        this.code = ERROR_CODE_PREFIX + number;
+        this.description = s;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    //TODO
+    //GetCodeWithprefix()
+    //getFormattedDescriptionWithCode
+    //getFormattedDescription
+
+}
