b6a6ea9398a1b29d376883e812bf5fa938452746
Kyle Smith <73062855+vertanzil@users.noreply.github.com>
2025-02-03 06:58:55 +0000

    Commit

--- src/main/java/io/github/vertanzil/Main.java
+++ src/main/java/io/github/vertanzil/Main.java
@@ -2,6 +2,11 @@ package io.github.vertanzil;
 
 public class Main {
     public static void main(String[] args) {
-    System.out.print(Messages.Message_004.getCode() + " " + Messages.Message_004.getDescription());
+        //Gets the error code only.
+        System.out.print(Messages.Message_001.getCode() + System.lineSeparator());
+        //gets the formatted description and reason.
+        System.out.print(Messages.Message_001.getFormattedDescription() + System.lineSeparator());
+        //Gets the formatted description with code and reason.
+        System.out.print(Messages.Message_001.GetFormattedDescriptionWithCode() + System.lineSeparator());
     }
 }
--- src/main/java/io/github/vertanzil/Messages.java
+++ src/main/java/io/github/vertanzil/Messages.java
@@ -1,37 +1,41 @@
 package io.github.vertanzil;
 
+import java.text.Format;
+
 public enum Messages {
     Message_001("0001", "Files not found."),
     Message_002("0002", "No connection to the database"),
     Message_003("0003", "Server offline"),
-    Message_004("0004", "Not found"),
-    ;
+    Message_004("0004", "Not found"),;
 
     private String code;
     private String description;
-    public static final String ERROR_CODE_PREFIX = "BCV";
+    private static final String ERROR_CODE_PREFIX = "BCV";
 
     private void Error(String code, String description) {
         this.code = code;
         this.description = description;
     }
-
     Messages(String number, String s) {
         this.code = ERROR_CODE_PREFIX + number;
         this.description = s;
     }
-
     public String getDescription() {
         return description;
     }
-
     public String getCode() {
         return code;
     }
+    private String getErrorCodePrefix(){
+        return ERROR_CODE_PREFIX ;
+    }
 
-    //TODO
-    //GetCodeWithprefix()
-    //getFormattedDescriptionWithCode
-    //getFormattedDescription
+    public String GetFormattedDescriptionWithCode(){
+        return getCode() + " " + getDescription();
+    }
+
+    public String getFormattedDescription(){
+        return getErrorCodePrefix() + " " + getDescription();
+    }
 
 }
